142176f43afa31760e5b377a7de6861467b70a5f
JoeTrung <161849281+JoeTSchwarz@users.noreply.github.com>
2024-11-04 12:04:47 +0100

    Update build.bat

--- nodes/JFX/build.bat
+++ nodes/JFX/build.bat
@@ -1,3 +1,3 @@
 cd .\classes
-jar -cvfme ../JfxODBServer.jar manifest.mf JFXODBServer *.class ../resources/*.css ../resources/*.fxml ../icons/*.gif > ../log.txt
+jar -cvfme ../JfxODBServer.jar ../resources/manifest.mf JFXODBServer *.class ../resources/*.css ../resources/*.fxml ../icons/*.gif > ../log.txt
 cd ..\
