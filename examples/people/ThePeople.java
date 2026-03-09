7144f36903b1d05c8e13fa0a166288a2e3d10437
JoeTrung <161849281+JoeTSchwarz@users.noreply.github.com>
2026-03-09 13:46:36 +0100

    Add files via upload

--- examples/people/ThePeople.java
+++ examples/people/ThePeople.java
@@ -5,7 +5,7 @@ import joeapp.mvc.SWINGLoader;
 //The ThePeople in SWING_MVC View
 public class ThePeople {
   public ThePeople(String... argv) throws Exception {
-    SWINGLoader ml = new SWINGLoader("resources/People.txt", 
+    SWINGLoader ml = new SWINGLoader("resources/_People_.txt", 
                                      "PeopleController",
                                      argv);
     ((JFrame) ml.load()).setVisible(true);
@@ -13,8 +13,8 @@ public class ThePeople {
   public static void main(String... a) throws Exception {
     UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
     String[] argv = a;
-    if (a.length == 0) argv = new String[] { "localhost:9999" };
-    else argv[0] = "localhost:"+argv[0];
+    if (a.length == 0) argv = new String[] { "192.168.0.70@9999" };
+    else argv[0] = "192.168.0.70@"+argv[0];
     //  
     new ThePeople(argv);
   }
